f6638c2fb5bfb983fd117f4f178bda31e03c965c
Matyrobbrt <matyrobbrt@gmail.com>
2022-01-07 22:26:41 +0200

    Disable a test module
    Signed-off-by: Matyrobbrt <matyrobbrt@gmail.com>

--- src/main/java/com/matyrobbrt/lib/dev_tests/TestModule.java
+++ src/main/java/com/matyrobbrt/lib/dev_tests/TestModule.java
@@ -27,10 +27,7 @@
 
 package com.matyrobbrt.lib.dev_tests;
 
-import com.matyrobbrt.lib.MatyLib;
-import com.matyrobbrt.lib.annotation.RL;
 import com.matyrobbrt.lib.module.IModule;
-import com.matyrobbrt.lib.module.Module;
 import com.matyrobbrt.lib.module.ModuleHelper;
 import com.matyrobbrt.lib.registry.annotation.AutoBlockItem;
 import com.matyrobbrt.lib.registry.annotation.RegisterBlock;
@@ -40,7 +37,7 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Blocks;
 import net.minecraft.tileentity.TileEntityType;
 
-@Module(id = @RL(modid = MatyLib.MOD_ID, path = "test"))
+// @Module(id = @RL(modid = MatyLib.MOD_ID, path = "test"))
 public class TestModule extends ModuleHelper implements IModule {
 
 	@AutoBlockItem
